9f366b09f6af96ceb3de6ac7efa69fb9ed92b1d0
Yuvraj Singh <yuvi.singh02@yahoo.com>
2024-03-30 16:32:29 +0100

    updated logstash and filebeat commands

--- Elastic Stack/elk-stack-install.sh
+++ Elastic Stack/elk-stack-install.sh
@@ -2,7 +2,7 @@
 
 # To complete this tutorial, you will need the following:
 # 1. An Ubuntu 22.04 server with 4GB RAM and 2 CPUs set up with a non-root sudo user.
-# 2. OpenJDK 11 installed. See the section Installing the Default JRE/JDK in our guide How
+# 2. OpenJDK 11 installed. See the section Installing the Default JRE/JDK
 sudo apt install openjdk-11-jre-headless
 # 3. Nginx installed on your server, which we will configure later in this guide as a reverse-proxy for Kibana. 
 # - 3.1. Installing Nginx
@@ -27,7 +27,7 @@ sudo apt install openjdk-11-jre-headless
     sudo systemctl disable nginx
     sudo systemctl enable nginx
 
-# 4. Remember to add import security groups such as opening HTTP access on port 80
+# 4. Remember to add security groups such as opening HTTP access on port 80
 
 # ----------------- ElasticSearch ----------------
 # Installing and configuring ElasticSearch
@@ -83,3 +83,55 @@ sudo ufw delete allow 'Nginx HTTP'
 # Example - http://10.196.36.199/status
 
 
+# -------------------------- Logstash ----------------------------------
+# Installing & Configuring logstash
+
+# Install Logstash
+sudo apt install logstash
+
+# Create a configuration file including the inputs and output parameters
+sudo nano /etc/logstash/conf.d/first-pipeline.conf
+
+# Test Logstash configuration with this command:
+# Expected Output --> Config Validation Result: OK. 
+# Along with some warnings regarding java version
+sudo -u logstash /usr/share/logstash/bin/logstash --path.settings/etc/logstash -t
+
+# Start and enable the logstash
+sudo systemctl start logstash
+sudo systemctl enable logstash
+
+
+# -------------------------- Filebeat -----------------------------------------
+# Installing and Configuring Filebeat
+
+# Install filebeat
+sudo apt install filebeat
+
+# Edit configuration to use logstash as output
+sudo nano /etc/filebeat/filebeat.yml
+
+# In this tutorial we will use the system module, which collects and parses logs created by the system logging service of
+# common Linux distributions.
+sudo filebeat modules enable system
+sudo filebeat modules list
+
+# To load the ingest pipeline for the system module, enter the following command:
+sudo filebeat setup --pipelines --modules system
+
+# Load the template
+sudo filebeat setup --index-management -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["localhost:9200"]'
+
+# load dashboards when Logstash is enabled, you need to disable the Logstash output and enable
+# Elasticsearch output:
+sudo filebeat setup -E output.logstash.enabled=false -E output.elasticsearch.hosts=['localhost:9200'] -E setup.kibana.host=localhost:5601
+
+# Start and enable the filebeat
+sudo systemctl start filebeat
+sudo systemctl enable filebeat
+
+# To verify that Elasticsearch is indeed receiving this data, query the Filebeat index with this command:
+curl -XGET 'http://localhost:9200/filebeat-*/_search?pretty'
+
+# If your output shows 0 total hits, Elasticsearch is not loading any logs under the index you
+# searched for
